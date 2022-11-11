1045786e948e60bc11cc4f495b2a4264169e2014
Tiger Taylor <tigertaylor1024@gmail.com>
2022-11-11 12:55:34 +0000

    Removed @next/font to fix styling issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,11 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Authentication from "../components/Authentication";
 
-import { Montserrat } from "@next/font/google";
-
-const montserrat = Montserrat({ subsets: ["latin-ext"] });
-
 function MyApp({ Component, pageProps }: AppProps) {
   const protectedRoutes = ["/dashboard"];
 
   return (
-    <div className={montserrat.className}>
+    <div>
       <Authentication protectedRoutes={protectedRoutes}>
         <Component {...pageProps} />
       </Authentication>
