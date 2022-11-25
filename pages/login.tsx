c02e192d91e880ac1518850d54f98bad94132730
Tiger Taylor <tigertaylor1024@gmail.com>
2022-11-25 18:48:37 +0000

    Move authentication to external server

--- pages/login.tsx
+++ pages/login.tsx
@@ -33,7 +33,7 @@ const Login: NextPage = () => {
 
     const jsonData = JSON.stringify(data);
 
-    const endpoint = "/api/auth/login";
+    const endpoint = "api.keysafe.info/auth/login";
 
     const options = {
       method: "POST",
