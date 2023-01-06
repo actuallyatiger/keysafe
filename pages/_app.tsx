6eaf3968e0eb8e0bfca88de431c620c5e15ce4f7
Tiger Taylor <tigertaylor1024@gmail.com>
2023-01-06 01:10:47 +0000

    Removed unnecessary Authentication wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,10 @@
-import Authentication from "components/Authentication";
 import type { AppProps } from "next/app";
 import "styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const protectedRoutes = ["/dashboard"];
-
   return (
     <div>
-      <Authentication protectedRoutes={protectedRoutes}>
-        <Component {...pageProps} />
-      </Authentication>
+      <Component {...pageProps} />
     </div>
   );
 }
